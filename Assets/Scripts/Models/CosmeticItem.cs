20a887181e2c0b19277b0f7004a798ede177b476
bharatipatil1102 <bharati.patil250896@gmail.com>
2026-02-17 15:25:34 +0530

    Add Assets/Scripts/Models/CosmeticItem.cs

---
+++ Assets/Scripts/Models/CosmeticItem.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CodeForgeRush.Models
+{
+    [Serializable]
+    public sealed class CosmeticItem
+    {
+        public string Id;
+        public string Name;
+        public int PriceCoins;
+    }
+}
